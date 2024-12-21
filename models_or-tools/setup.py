20eff8387993fa634d34a1204e390511013c5677
Alexandre Couedelo <a.couedelo@efg.gg>
2024-12-21 14:27:00 +0000

    fix: installation of knsp module

--- models_or-tools/setup.py
+++ models_or-tools/setup.py
@@ -2,5 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="knsp",
-    packages=find_packages('knsp')
+    version="0.0.1",
+    packages=find_packages('knsp'),
+    package_dir={'': 'knsp'}
 )
